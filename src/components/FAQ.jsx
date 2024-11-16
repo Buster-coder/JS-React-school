c9e684f9ab7e1cbe5a7da373fa83ae3893aa6b55
Buster-coder <gokhan_89@hotmail.com>
2024-11-16 16:10:31 +0100

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,83 +1,209 @@
-import React from 'react'
-import PhoneGreen from '../assets/image/icongreen.svg'
-import PhoneIcon from '../assets/image/iconphone.svg'
+import React, { useEffect } from 'react';
+import PhoneGreen from '../assets/image/icongreen.svg';
+import PhoneIcon from '../assets/image/iconphone.svg';
 
 const FAQ = () => {
-  return (
-    <section class="sixth-background">
-    <div class="phone-container">
-        <div class="the-left-text">
-         <h2>Any questions?<br />Check out the FAQs</h2>
-         <p class="phony-p">Still have unanswered questions and need to get<br />in touch?</p>
-         <div class="call-feature">
-           <div class="the-phone"><img src={PhoneIcon} alt="" />
-           <p class="phone-p">Still have questions?</p>
-           <a href="" class="the-phone-1">
-           <span>Contact us</span>
-           <i class="fa-solid fa-arrow-right"></i>
-           </a>
-         </div>
-         </div>
-         <div class="call-feature-2">
-           <div class="the-phones"><img src={PhoneGreen} alt="" />
-           <p class="phone-p">Dont like phone calls?</p>
-           <a href="#" class="the-phone-1">
-           <span class="phones-green">Contact us</span>
-           <i class="fa-solid fa-arrow-right"></i>
-           </a>
-         </div>
-         </div>
-        </div>
-        
-        <div class="the-right-stuff">
-        <ul class="the-right-side">
-          <li>
-            <input type="radio" name="accordion" id="first-collapse" class="input-radio" />
-            <label for="first-collapse" class="lab-text">Is any of my personal information stored in the App<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-          <li>
-            <input type="radio" name="accordion" id="second-collapse" class="input-radio" />
-            <label for="second-collapse" class="lab-text">What formats can i download my transaction history in?<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-          <li>
-            <input type="radio" name="accordion" id="third-collapse" class="input-radio" />
-            <label for="third-collapse" class="lab-text">Can i schedule future transfers?<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-          <li>
-            <input type="radio" name="accordion" id="fourth-collapse" class="input-radio" />
-            <label for="fourth-collapse" class="lab-text">When can i use Banking App services?<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-          <li>
-            <input type="radio" name="accordion" id="fifth-collapse" class="input-radio" />
-            <label for="fifth-collapse" class="lab-text">Can i create my own password that is easy for me to remeber?<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-          <li>
-            <input type="radio" name="accordion" id="sixth-collapse" class="input-radio" />
-            <label for="sixth-collapse" class="lab-text">What happens if i forget or lose my password?<span class="circles"><i class="fa-solid fa-chevron-up"></i></span></label>
-            <div class="all-content">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis labore! Numquam quasi reprehenderit praesentium saepe libero ea voluptate. Nostrum nam ullam omnis praesentium?</p>
-            </div>
-          </li>
-        </ul>
-        </div>
-        </div>
-</section>
-  )
-}
+  useEffect(() => {
+    let lastChecked = null;
 
-export default FAQ
+
+    const radioButtons = document.querySelectorAll('.input-radio');
+
+
+    radioButtons.forEach((radio) => {
+      radio.addEventListener('click', function () {
+        if (radio === lastChecked) {
+          radio.checked = false;
+          lastChecked = null;
+        } else {
+          lastChecked = radio;
+        }
+      });
+    });
+
+
+    return () => {
+      radioButtons.forEach((radio) => {
+        radio.removeEventListener('click', () => { });
+      });
+    };
+  }, []);
+
+  return (
+    <section className="sixth-background">
+      <div className="phone-container">
+        <div className="the-left-text">
+          <h2>
+            Any questions?
+            <br />
+            Check out the FAQs
+          </h2>
+          <p className="phony-p">
+            Still have unanswered questions and need to get
+            <br />
+            in touch?
+          </p>
+          <div className="call-feature">
+            <div className="the-phone">
+              <img src={PhoneIcon} alt="" />
+              <p className="phone-p">Still have questions?</p>
+              <a href="" className="the-phone-1">
+                <span>Contact us</span>
+                <i className="fa-solid fa-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+          <div className="call-feature-2">
+            <div className="the-phones">
+              <img src={PhoneGreen} alt="" />
+              <p className="phone-p">Don’t like phone calls?</p>
+              <a href="#" className="the-phone-1">
+                <span className="phones-green">Contact us</span>
+                <i className="fa-solid fa-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="the-right-stuff">
+          <ul className="the-right-side">
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="first-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="first-collapse" className="lab-text">
+                Is any of my personal information stored in the App
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="second-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="second-collapse" className="lab-text">
+                What formats can I download my transaction history in?
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="third-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="third-collapse" className="lab-text">
+                Can I schedule future transfers?
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="fourth-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="fourth-collapse" className="lab-text">
+                When can I use Banking App services?
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="fifth-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="fifth-collapse" className="lab-text">
+                Can I create my own password that is easy for me to remember?
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+            <li>
+              <input
+                type="radio"
+                name="accordion"
+                id="sixth-collapse"
+                className="input-radio"
+              />
+              <label htmlFor="sixth-collapse" className="lab-text">
+                What happens if I forget or lose my password?
+                <span className="circles">
+                  <i className="fa-solid fa-chevron-up"></i>
+                </span>
+              </label>
+              <div className="all-content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Dolorem delectus nesciunt veritatis ipsa deserunt nemo facilis
+                  labore! Numquam quasi reprehenderit praesentium saepe libero
+                  ea voluptate. Nostrum nam ullam omnis praesentium?
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
